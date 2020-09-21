204761e7b7e56045bddbc6f108e3aa3e47f43f7b
oubre <59916969+LLami1337@users.noreply.github.com>
2020-09-22 00:22:06 +1000

    Create num.cpp

---
+++ 1Tournament/B - Finite Field (Easy version)/num.cpp
@@ -0,0 +1,12 @@
+#include "num.h"
+
+Num::Num(int value, int modulo) {
+	this->value = value % modulo;
+	this->modulo = modulo;
+}
+
+Num::Num(const Num& other) {
+	this->modulo = 0;
+	this->value = other.value;
+}
+	
