75af1f621939b352f120f2ca575eb1d49588b07d
oubre <59916969+LLami1337@users.noreply.github.com>
2020-09-21 18:21:52 +1000

    Update class PrimeNumberGenerator.cpp

--- 1Tournament/A - PrimeNumberGenerator/class PrimeNumberGenerator.cpp
+++ 1Tournament/A - PrimeNumberGenerator/class PrimeNumberGenerator.cpp
@@ -1,1 +1,27 @@
+class PrimeNumberGenerator {
 
+public:
+    int start;
+    explicit PrimeNumberGenerator(int startNumber) {
+        start = startNumber;
+    }
+
+    int GetNextPrime() {
+        while (CheckPrime(start) == false) {
+            start++;
+        }
+        int res = start;
+        start++;
+        return res;
+    }
+
+    bool CheckPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+        return true;
+        
+    }
+
+};
