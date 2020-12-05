3bddaf80e608d3c3e4fb6e2283e7b8aedfb7416f
oubre <59916969+LLami1337@users.noreply.github.com>
2020-12-05 21:23:20 +1000

    init with catch tests

---
+++ 2Tournament/hashmap/cmake-build-debug/CMakeFiles/hashmap_v2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hashmap_v2.dir/main.cpp.o"
+  "hashmap_v2"
+  "hashmap_v2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hashmap_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
